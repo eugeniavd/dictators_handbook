bfd541a32d551cff7d223ab6c5b9548831c9992a
eugeniavd <evgeniia.vdovichenko@studio.unibo.it>
2025-02-06 19:02:51 +0100

    fixing

--- utilities/functions.js
+++ utilities/functions.js
@@ -6,17 +6,33 @@
 
 /**
  * Switches the design theme by updating the stylesheet link.
+/**
  * @param {string} stylePath - Path to the CSS file.
  */
 function switchStyle(stylePath) {
   const themeStylesheet = document.getElementById('themeStylesheet');
   if (themeStylesheet) {
     themeStylesheet.href = stylePath;
+    // Saving design theme in the local storage
+    localStorage.setItem('selectedTheme', stylePath);
     console.log(`Switched style to: ${stylePath}`);
   } else {
     console.error('Theme stylesheet element with id "themeStylesheet" not found.');
   }
 }
+
+// Applying the choosen theme to other pages
+document.addEventListener('DOMContentLoaded', () => {
+  const savedTheme = localStorage.getItem('selectedTheme');
+  if (savedTheme) {
+    const themeStylesheet = document.getElementById('themeStylesheet');
+    if (themeStylesheet) {
+      themeStylesheet.href = savedTheme;
+      console.log(`Applied saved theme: ${savedTheme}`);
+    }
+  }
+});
+
 
 document.addEventListener('DOMContentLoaded', () => {
   // Add hover functionality for dropdown submenus
