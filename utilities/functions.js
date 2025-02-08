313ec6a476901df1f7a1322a79eab423f88cc1e5
eugeniavd <evgeniia.vdovichenko@studio.unibo.it>
2025-02-08 16:55:34 +0100

    fixing2

--- utilities/functions.js
+++ utilities/functions.js
@@ -5,19 +5,33 @@
  */
 
 /**
- * Switching design themes
- * @param {string} stylePath path to the css file 
+ * Switches the design theme by updating the stylesheet link.
+/**
+ * @param {string} stylePath - Path to the CSS file.
  */
 function switchStyle(stylePath) {
   const themeStylesheet = document.getElementById('themeStylesheet');
   if (themeStylesheet) {
     themeStylesheet.href = stylePath;
+    // Saving design theme in the local storage
+    localStorage.setItem('selectedTheme', stylePath);
     console.log(`Switched style to: ${stylePath}`);
   } else {
     console.error('Theme stylesheet element with id "themeStylesheet" not found.');
   }
 }
 
+// Applying the choosen theme to other pages
+document.addEventListener('DOMContentLoaded', () => {
+  const savedTheme = localStorage.getItem('selectedTheme');
+  if (savedTheme) {
+    const themeStylesheet = document.getElementById('themeStylesheet');
+    if (themeStylesheet) {
+      themeStylesheet.href = savedTheme;
+      console.log(`Applied saved theme: ${savedTheme}`);
+    }
+  }
+});
 
 
 document.addEventListener('DOMContentLoaded', () => {
